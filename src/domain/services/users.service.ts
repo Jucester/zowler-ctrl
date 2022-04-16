03acf9f0ec8355b71fdbcc4ccd473c0162bb274d
Jucester <juceld@gmail.com>
2022-04-15 23:20:27 -0400

    Error response with better format

--- src/domain/services/users.service.ts
+++ src/domain/services/users.service.ts
@@ -1,27 +1,45 @@
 import UsersRepository from "../../infrastructure/mongoose/repositories/users.repository";
 class UsersService {
+  private _repo: any;
 
-    private _repo: any;
+  constructor(public readonly repo: any) {
+    this._repo = repo;
+  }
 
-    constructor(public readonly repo: any) {
-        this._repo = repo;
+  async findAll(queryParams: any) {
+    const [res, error] = await this._repo.findAll();
+
+    if (error) {
+      return {
+        statusCode: 500,
+        body: {
+          error: error,
+        },
+      };
     }
 
-    async findAll(queryParams: any) {
-        const res = await this._repo.findAll();
-        return {
-            statusCode: 200,
-            body: res,
-        }
-    }
+    return {
+      statusCode: 200,
+      body: res,
+    };
+  }
 
-    async create(entity: any) {
-        const res = await this._repo.create(entity);
-        return {
-            statusCode: 200,
-            body: res,
-        }
+  async create(entity: any) {
+    const [res, error] = await this._repo.create(entity);
+
+    if (error) {
+      return {
+        statusCode: 500,
+        body: {
+          error: error
+        },
+      };
     }
+    return {
+      statusCode: 200,
+      body: res,
+    };
+  }
 }
 
 export default new UsersService(UsersRepository);
--- src/infrastructure/mongoose/repositories/users.repository.ts
+++ src/infrastructure/mongoose/repositories/users.repository.ts
@@ -1,21 +1,30 @@
-import { Model } from 'mongoose';
-import User from '../schemas/users/user.schema';
+import { Model } from "mongoose";
+import User from "../schemas/users/user.schema";
 
 class UserRepository {
+  private _model: Model<any, any>;
 
-    private _model: Model<any, any>;
+  constructor(model: Model<any, any>) {
+    this._model = model;
+  }
 
-    constructor(model: Model<any, any>) {
-        this._model = model;
+  async findAll() {
+    try {
+      const res = await this._model.find();
+      return [res, null];
+    } catch (error) {
+      return [null, error];
     }
+  }
 
-    async findAll() {
-        return await this._model.find();
+  async create(body: any) {
+    try {
+      const res = await this._model.create(body);
+      return [res, null];
+    } catch (error) {
+      return [null, error];
     }
-
-    async create(body: any) {
-        return await this._model.create(body);
-    }
+  }
 }
 
 export default new UserRepository(User);
